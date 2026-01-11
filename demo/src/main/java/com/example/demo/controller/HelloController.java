0c0741553d8ed5459561abedd18326d7014676a3
dok32 <dok32@example.com>
2026-01-11 22:51:11 +0900

    add swagger

--- demo/src/main/java/com/example/demo/controller/HelloController.java
+++ demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -1,5 +1,7 @@
-package com.example.demo;
+package com.example.demo.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.Collections;
@@ -8,6 +10,8 @@ import java.util.Map;
 @RestController
 public class HelloController {
 
+    @Operation(summary = "Hello World", description = "Returns a simple 'Hello, World!' message.")
+    @ApiResponse(responseCode = "200", description = "Successfully retrieved the message.")
     @GetMapping("/")
     public Map<String, String> hello() {
         return Collections.singletonMap("message", "Hello, World!");
